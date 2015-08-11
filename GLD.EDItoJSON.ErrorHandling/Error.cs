a7049ab628f5e2c413c299799ee2b941a0f91f33
Leonid Ganeline <leo.gan.57@gmail.com>
2015-08-11 13:00:54 -0700

    first workable version

--- GLD.EDItoJSON.ErrorHandling/Error.cs
+++ GLD.EDItoJSON.ErrorHandling/Error.cs
@@ -8,16 +8,26 @@ namespace GLD.EDItoJSON.ErrorHandling
     /// </summary>
     public static class Errors
     {
-        public static List<string> Log = new List<string>();
+        public static List<string> Logs = new List<string>();
 
         /// <summary>
-        /// Outputs composed Log.
+        /// Outputs composed Logs.
         /// </summary>
-        public static void Report() {}
+        public static void Report()
+        {
+            if (Logs.Count != 0)
+            {
+                Console.WriteLine("Logged Errors:");
+                foreach (var log in Logs)
+                    Console.WriteLine(log);
+            }
+            Console.WriteLine("Press Enter to finish.");
+            Console.ReadLine();
+        }
 
         public static void NewError(string errorText)
         {
-            Log.Add(errorText);
+            Logs.Add(errorText);
         }
        /// <summary>
        /// if validation failed, add a new error. Do not throw an exception.
--- GLD.EDItoJSON.Parser/Parser.cs
+++ GLD.EDItoJSON.Parser/Parser.cs
@@ -19,14 +19,15 @@ namespace GLD.EDItoJSON.Parser
         /// <param name="args"></param>
         static void Main(params string[] args)
         {
-            if (args == null || args.Length != 2)
+            if (args == null || args.Length != 1)
             {
-                Console.WriteLine("Wrong call. Usage: >Parser inputEDIfile outputJSONfile");
+                Console.WriteLine("Wrong call. Usage: >Parser inputEDIfile");
                 return;
             }
-            Console.WriteLine("Parser Started: Input file:{0}, OutputFile{1}", args[0], args[1]);
+            Console.WriteLine("Parser Started: Input file:{0}", args[0]);
             var inputFileName = args[0];
-            var outputFileName = args[1];
+            var outputFileName = inputFileName + ".Output.js";
+            var errorsFileName = inputFileName + ".Errors.js";
 
             var interchange = new Interchange {SegmentSeparator = GetSegmentSeparator(inputFileName), DataElementSeparator = GetDataElementSeparator(inputFileName), DataComponentSeparator = GetDataComponentSeparator(inputFileName)};
 
@@ -36,7 +37,7 @@ namespace GLD.EDItoJSON.Parser
             var currentDocument = new Document();
 
             // traverse all segments:
-            interchange.ISASegment = new ISASegment(segments[0]);
+            interchange.ISASegment = new ISASegment(segments[0].Elements);
 
             for (var i = 1; i < segments.Count; i++)
             {
@@ -44,22 +45,22 @@ namespace GLD.EDItoJSON.Parser
                 {
                     case "ST":
                         currentDocument = new Document(); 
-                        currentDocument.STSegment = new STSegment(segments[i]);
+                        currentDocument.STSegment = new STSegment(segments[i].Elements);
                         break;
                     case "SE":
-                        currentDocument.SESegment = new SESegment(segments[i]);
+                        currentDocument.SESegment = new SESegment(segments[i].Elements);
                         currentGroup.Documents.Add(currentDocument);
                         break;
                     case "GS":
                         currentGroup = new Group(); 
-                        currentGroup.GSSegment = new GSSegment(segments[i]);
+                        currentGroup.GSSegment = new GSSegment(segments[i].Elements);
                         break;
                     case "GE":
-                        currentGroup.GESegment = new GESegment(segments[i]);
+                        currentGroup.GESegment = new GESegment(segments[i].Elements);
                         interchange.Groups.Add(currentGroup);
                         break;
                     case "IEA":
-                        interchange.IEASegment = new IEASegment(segments[i]);
+                        interchange.IEASegment = new IEASegment(segments[i].Elements);
                         break;
                     default:
                         currentDocument.Segments.Add(segments[i]);
@@ -67,9 +68,10 @@ namespace GLD.EDItoJSON.Parser
                 }
             }
             // Check if errors happened and Validate the top level EDI structure, like missed IEA segment.
-            if (!interchange.IsValid() || Errors.Log.Count != 0)
+            if (!interchange.IsValid() || Errors.Logs.Count != 0)
             {
                 Errors.Report();
+                File.WriteAllText(errorsFileName, JsonConvert.SerializeObject(Errors.Logs));
                 return;
             }
 
@@ -78,6 +80,11 @@ namespace GLD.EDItoJSON.Parser
 
             // output JSON:
             File.WriteAllText(outputFileName, serializedInterchange);
+
+            Console.WriteLine("Success.");
+            Console.WriteLine("Press Enter to finish.");
+            Console.ReadLine();
+
         }
 
     
@@ -93,15 +100,14 @@ namespace GLD.EDItoJSON.Parser
         private static Segment ParseSegment(string line, char dataElementSeparator)
         {
             var elements = line.Split(dataElementSeparator);
-            Errors.Validate(() => elements.Length == 0, string.Format("Line {0} cannot be parsed. It cannot be splitted with {1} separator.", line, dataElementSeparator));
-            Errors.Validate(() => elements.Length == 1,
+            Errors.Validate(() => elements.Length != 0, string.Format("Line {0} cannot be parsed. It cannot be split with {1} separator.", line, dataElementSeparator));
+            Errors.Validate(() => elements.Length != 1,
                 string.Format("Line {0} cannot be parsed. With {1} separator it has only one element.", line,
                     dataElementSeparator));
-            
-            var segment = new Segment(elements[0]);
-            segment.Elements = new string[elements.Length];
-            Array.Copy(elements, 1, segment.Elements, 0, elements.Length - 1);
-            return segment;
+            for (var index = 0; index < elements.Length; index++)
+                elements[index] = elements[index].Trim();
+
+            return new Segment(elements);
         }
 
         private static char GetSegmentSeparator(string fileName)
